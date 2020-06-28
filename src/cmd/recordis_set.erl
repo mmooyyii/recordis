fb8d389d07c86e1e78ade04df2ce487c6cffd967
yimo <moyishi213@gmail.com>
2020-06-28 22:30:14 +0800

    match sets

--- src/cmd/recordis_set.erl
+++ src/cmd/recordis_set.erl
@@ -5,22 +5,17 @@
 -export([is_member/2, set/2, get/1]).
 -include("recordis.hrl").
 
--define(EmptySet, {set, 0, 16, 16, 8, 80, 48,
-    {[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []},
-    {{[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []}}}).
 
 %% read
 is_member(Key, Element) ->
     [<<"SISMEMBER">>, Key, Element].
 
 % write
-set(_Key, ?EmptySet) ->
-    #redis_cmd{};
 set(Key, Set) ->
-    #redis_cmd{
-        cmd = [<<"SADD">>, Key] ++ recordis_type:redis(set, Set)
-    }.
-
+    case sets:size(Set) of
+        0 -> #redis_cmd{};
+        _ -> #redis_cmd{cmd = [<<"SADD">>, Key] ++ recordis_type:redis(set, Set)}
+    end.
 
 get(Key) ->
     #redis_cmd{cmd = [<<"SMEMBERS">>, Key], transfer = {sets, from_list}}.
